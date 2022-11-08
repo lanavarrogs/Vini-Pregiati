d763b9b6fa524817f86f64844bd5a510041b4a7b
Luis Navarro <lanavarrogs@gmail.com>
2022-11-08 10:05:53 -0600

    production

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const dbConfig = require("./app/config/db.config");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "http://vinipregati.eastus.cloudapp.azure.com:80",
 };
 
 app.use(cors(corsOptions));
