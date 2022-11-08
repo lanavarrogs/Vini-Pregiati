53b68829498c4f7530d7230152abc6337fc8cc45
Luis Navarro <lanavarrogs@gmail.com>
2022-11-07 20:10:39 -0600

    Commit made from VS Code Azure Static Web Apps

--- client/src/components/ListWines.jsx
+++ client/src/components/ListWines.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import Wine from './Wine'
 
 const ListWines = () => {
   return (
     <div  className='container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center '>
       <div className='md:grid grid-cols-3 gap-3'>
-        <Link to={'/description/1'}>
-          <Wine/>
-        </Link>
         <Wine/>
         <Wine/>
         <Wine/>
--- client/src/components/Wine.jsx
+++ client/src/components/Wine.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import wine from '../assets/white_wine.png'
 
 const Wine = () => {
@@ -9,6 +10,7 @@ const Wine = () => {
               <h3 className='text-white font-bold text-xl'>Vino Rojo</h3>
               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum totam dolorem quibusdam sapiente ipsum nam, suscipit velit labore adipisci odio exercitationem non, quam harum saepe expedita alias accusamus itaque vel?</p>
               <p className='text-[#58D7C4]'>$560.55</p>
+              <Link className='flex justify-end m-3 ' to={'/description/1'}>Ver mas</Link>
           </div>
         </div>
     )
